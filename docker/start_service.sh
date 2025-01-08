b7436bdb0472cd8c9ea8ef81e21f3b766b61bf78
dsy3502 <290056547@qq.com>
2025-01-08 11:05:31 +0800

    add db sync

--- docker/start_service.sh
+++ docker/start_service.sh
@@ -5,6 +5,11 @@ set -ex
 echo "/usr/local/bin/gunicorn -c /etc/dingoops/gunicorn.py main:app" >/run_command
 
 mapfile -t CMD < <(tail /run_command | xargs -n 1)
+# kolla_extend_start
+if [[ "${!KOLLA_BOOTSTRAP[*]}" ]]; then
 
+    alembic -c ./db/alembic/alembic.ini upgrade head
+    exit 0
+fi
 echo "Running command: ${CMD[*]}"
 exec "${CMD[@]}"
